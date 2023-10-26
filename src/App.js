b23a31a47b0fc2698529df61df71b4d0b6ee8dd6
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-10-26 21:35:05 +0530

    VIDEO 13

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import {useState} from 'react';
+
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import User from './User'
 import User2 from './User2';
 import User3 from './User3';
  
-function App() {
-   const [data, setData] = useState(0)
-   function updateData(){
-    setData(data+1)
-   }
-   console.warn("______");
-  return (
-    <div className="App">
-       <h1>{data}</h1>
-       <button onClick= {updateData}>Update Data</button>
-       </div>
-  );
+class App extends Component {
+   
+  constructor()
+  {
+    super();
+    this.state={
+      data:1
+    }
+  }
+
+  apple()
+  {
+    this.setState({data:this.state.data+1})
+  }
+
+  render() 
+  {
+    return (
+      <div className='App'>
+        <h1> Hello world!</h1>
+        <button onclick= {()=> this.apple()}>UPDATE DATA</button>
+      </div>
+    );
+  }
+    
 }
 
 export default App;
