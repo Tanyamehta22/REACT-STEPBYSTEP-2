d46443ad23bcd09d8421b7d2f5c6b2496c0644a8
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-10-26 20:50:29 +0530

    VIDEO-11

--- src/App.js
+++ src/App.js
@@ -3,10 +3,28 @@ import './App.css';
 import User from './User'
 import User2 from './User2';
 import User3 from './User3';
+ 
 function App() {
+  // VARIABLE TAKING
+  let dat = "anil sindhu"
+  
+  function apple()
+  {
+    data = "peter"
+    alert(data);
+  }
+
   return (
     <div className="App">
-       <h1>Hello World</h1>
+       <h1>{anil sidhu}</h1>
+       {/* <button onClick={apple}>click Me</button> */}
+
+       {/* WE USE ARROW FN TO DIRECTLY USE ALERT */}
+       <button onClick={()=>alert("hello")}>click Me</button>
+
+       {/* WE CAN ALSO CALL APPLE USING ARROW FUN */}
+       <button onClick={()=>apple()}>click Me</button>
+
        <User/>
        <User2/> 
        <User3/>
