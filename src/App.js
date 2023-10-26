fd4ccd16e94753860c0590419124252c147a69c8
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-10-26 21:17:46 +0530

    VIDEO 12

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import User from './User'
@@ -5,29 +6,15 @@ import User2 from './User2';
 import User3 from './User3';
  
 function App() {
-  // VARIABLE TAKING
-  let dat = "anil sindhu"
-  
-  function apple()
-  {
-    data = "peter"
-    alert(data);
-  }
-
+   const [data, setData] = useState(0)
+   function updateData(){
+    setData(data+1)
+   }
+   console.warn("______");
   return (
     <div className="App">
-       <h1>{anil sidhu}</h1>
-       {/* <button onClick={apple}>click Me</button> */}
-
-       {/* WE USE ARROW FN TO DIRECTLY USE ALERT */}
-       <button onClick={()=>alert("hello")}>click Me</button>
-
-       {/* WE CAN ALSO CALL APPLE USING ARROW FUN */}
-       <button onClick={()=>apple()}>click Me</button>
-
-       <User/>
-       <User2/> 
-       <User3/>
+       <h1>{data}</h1>
+       <button onClick= {updateData}>Update Data</button>
        </div>
   );
 }
