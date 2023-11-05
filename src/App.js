c31e88019fb85e9f7523f5865b853a68094de75a
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-11-06 01:34:03 +0530

    VIDEO 18

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import React , {useStae}from 'react'
+import React , {useState}from 'react'
  
 function App(){
-  const [status, setstatus]= React.usedState(true)
+  const [name,setName] = useState("");
+  const [tnc,setTnc]= useState(false);
+  const [interest, setinterest]= useState("");
+  function getFormData(e)
+  {
+    console.log(name,tnc,interest)
+    e.preventDefault()
+  }
+   
   return(
     <div className='App'>
-      {
-        status? <h1>Hello World!</h1> :null
-      }
-       
-      {/* <button onClick={()=> setstatus(false)}>HIDE</button>
-      <button onClick={()=> setstatus(true)}>SHOW</button> */}
-      <button onClick={()=> setstatus(!status)}>TOGGLE</button> 
-
+      <h1>HANDLE FORM IN REACT</h1>
+      <form onSubmit = {getFormData}>
+        <input type="text" placeholder='enter name' value={name} onChange={(e)=>setName(e.target.value)}/> <br/><br/>
+        <select onChange={(e)=> setinterest(e.target.checked)}>
+          <option>SELECT OPTIONS</option>
+          <option>MARVEL</option>
+          <option>DC</option>
+        </select><br/><br/>
+        <input type="checkbox" onChange={(e)=>setTnc(e.target.checked)}/> <span>Accept Terms and conditions</span> <br/> <br/>
+        <button type="submit">Submit</button>
+        <button type="submit">Clear</button>
+      </form>
     </div>
+     
+     
   );
 }
 
