7b5affc206e4db483c9b784a2e73766c40ae6170
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-11-23 23:06:02 +0530

    VIDEO-25

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import User5 from './USER5.JS';
+import React from 'react'
+class App extends React.Component{
+  constructor()
+  {
+    super();
+    this.state={
+      name:"anil"
+    }
 
-function App(){
-  const [name, setName]= React.useState("Anil")
-  return(
-    <div className='App'>
-      <h1>Render Method in React</h1>
-      <User5/>
-      {/*<button onClick={()=>setName("Sindu")}>UPDATE NAME</button> */}
-    </div>
-  );
-}
+
+     
+  }
+  componentDidMount()
+  {
+    console.warn("componentDidMount")
+  }
+  }
+  render()
+  {
+    console.warn("render")
+
+    return(
+      <div className='App'>
+        <h1>Component Did Mount {this.state.name}</h1>
+        <button onClick={()=>{this.setState({name: "Sidhu"})}}>Update Name</button>
+      </div>
+    );
+  }
+
+
+ 
 
 export default App;
  
