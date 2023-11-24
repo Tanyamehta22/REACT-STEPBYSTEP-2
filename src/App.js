d477a405806032cc6b0a61ba4d4567eb61292984
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-11-25 00:41:36 +0530

    VIDEO-30

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import React,{useState,useEffect} from 'react'
-function App(){
-  const [data,setdata]= useState("Anil")
+import "./App.css";
+import React, { useEffect ,useState} from "react";
+function App() {
+  const [count,setCount]= useState(0)
+  useEffect(() => {
+    console.warn("useEffect")
+  });
+  useEffect(() => {
+    console.warn("useEffect 1")
+  });
+
   return (
-    <div className='App'>
-      <h1>{data}</h1>
-      <button onClick={()=>setdata("Sidhu")}>Update Data</button>
+    <div className="App">
+      <h1>useEffect in React {count}</h1>
+      <button onClick={()=>setCount(count+1)}>Update Counter</button>
     </div>
   );
 }
- 
 
 export default App;
- 
- 
- 
-  
-   
-  
