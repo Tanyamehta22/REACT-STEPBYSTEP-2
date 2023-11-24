ab4816884dcb02ddec5aed4faea3686d7980091c
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-11-24 18:46:29 +0530

    VIDEO-27

--- src/App.js
+++ src/App.js
@@ -5,24 +5,20 @@ class App extends React.Component{
   constructor()
   {
     super();
-    console.log("constructor")
     this.state={
       count:0
     }
-
-     
-
-
      
   }
-  componentDidUpdate(preProps, preState, snapshot)
+   
+  }
+
+  shouldComponentUpdte()
   {
-    console.warn("componentDidUpdate",preState.count,this.state.count)
-    if(this.state.count<10)
+    console.warn("shouldComponentUpdate", this.state.count);
     {
-       this.setState({count:this.state.count+1})
+      return true;
     }
-  }
   }
   render()
   {
@@ -30,8 +26,10 @@ class App extends React.Component{
 
     return(
       <div className='App'>
-        <h1>Component Did Update{this.state.count}</h1>
-        <button onClick={()=>{this.setState({count:1})}}>Update Name</button>
+        <h1>Should Component Update {this.state.count} </h1>
+        <button onClick={()=>{history.setState({count:this.state.count+1})}}>UPDATE COUNTER</button>
+
+         
       </div>
     );
   }
