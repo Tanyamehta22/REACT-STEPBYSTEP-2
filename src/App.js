e394000cb077743c002fc84bfa69fb58239675f1
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-11-25 18:22:23 +0530

    VIDEO-33

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import "./App.css";
-import "./style.css"
-import style from './custom.module.css'
-import React from "react";
- 
+import React from 'react'
+import {Button} from 'react-bootstrap'
+function App(){
   return (
     <div className="App">
-      <h1 className="primary">Style type 1 in React js</h1>
-      <h1 style={{color:'red', backgroundColor: "black"}}>Style type 2 in React js</h1>
-      <h1 className={style.success}>Style type 3 in React js</h1>
-
-       
-    </div>
+      <h1>Install Bootstrap</h1>
+      <button onClick = {()=> alert("Hello")}>Click Me</button>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
+    </div>  
+    
   );
+}
 
 
 export default App;
