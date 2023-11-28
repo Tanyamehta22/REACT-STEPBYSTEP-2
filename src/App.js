3e7ef78a0156e39963ad3c2ebfce1febad3417b8
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-11-28 18:13:59 +0530

    VIDEO-34

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import "./App.css";
-import React from 'react'
-import {Button} from 'react-bootstrap'
-function App(){
+import React from "react";
+function App() {
+  // const students = ["Anil", "Sidhu", "Sam", "Peter"];
+  const students = [
+    { name: "anil", email: "anil@test.com", contact: 888 },
+    { name: "sidhu", email: "sidhu@test.com", contact: 111 },
+    { name: "sam", email: "sam@test.com", contact: 222 },
+    { name: "peter", email: "peter@test.com", contact: 333 },
+  ];
+  //map looping
+  // students.map((item)=>{
+  //   console.warn("My Name is:",item)
+  // });
+
+  // for(let i=0;i<students.length;i++)
+  // {
+  //   console.warn("in for loop My Name is:", students[i])
+  // }
   return (
     <div className="App">
-      <h1>Install Bootstrap</h1>
-      <button onClick = {()=> alert("Hello")}>Click Me</button>
-      <Button variant="primary">Primary</Button>{' '}
-      <Button variant="secondary">Secondary</Button>{' '}
-      <Button variant="success">Success</Button>{' '}
-      <Button variant="warning">Warning</Button>{' '}
-      <Button variant="danger">Danger</Button>{' '}
-      <Button variant="info">Info</Button>{' '}
-      <Button variant="light">Light</Button>{' '}
-      <Button variant="dark">Dark</Button>
-      <Button variant="link">Link</Button>
-    </div>  
-    
+      <h1>Handle Array with List</h1>
+      <table border='1'>
+      <tr>
+         <td>NAME</td>
+         <td>EMAIL</td>
+         <td>CONTACT</td>
+        </tr>
+      {students.map((data) => (
+        <tr>
+         <td>{data.name}</td>
+         <td>{data.email}</td>
+         <td>{data.contact}</td>
+        </tr>
+      ))}
+      </table>
+    </div>
   );
 }
-
 
 export default App;
