527d1bc308e10e2c97e6365a024fdefa33f4d218
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-11-28 18:42:40 +0530

    VIDEO-35

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
 import "./App.css";
 import React from "react";
+import {Table} from 'react-bootstrap'
 function App() {
-  // const students = ["Anil", "Sidhu", "Sam", "Peter"];
-  const students = [
+   
+  const users = [
     { name: "anil", email: "anil@test.com", contact: 888 },
     { name: "sidhu", email: "sidhu@test.com", contact: 111 },
-    { name: "sam", email: "sam@test.com", contact: 222 },
-    { name: "peter", email: "peter@test.com", contact: 333 },
+    { name: "sam", email: "sam@test.com", contact: 111},
+    { name: "peter", email: "peter@test.com", contact: 111},
   ];
-  //map looping
-  // students.map((item)=>{
-  //   console.warn("My Name is:",item)
-  // });
-
-  // for(let i=0;i<students.length;i++)
-  // {
-  //   console.warn("in for loop My Name is:", students[i])
-  // }
+   
+   
   return (
     <div className="App">
-      <h1>Handle Array with List</h1>
-      <table border='1'>
-      <tr>
-         <td>NAME</td>
-         <td>EMAIL</td>
-         <td>CONTACT</td>
-        </tr>
-      {students.map((data) => (
+      <h1>LIST WITH BOOTSTRAP TABLE</h1>
+      <Table striped variant="dark">
+        <tbody>
         <tr>
-         <td>{data.name}</td>
-         <td>{data.email}</td>
-         <td>{data.contact}</td>
+          <td>NAME</td>
+          <td>EMAIL</td>
+          <td>CONTACT</td>
         </tr>
+       
+      {users.map((item,i) => (
+       item.contact === '111' ?
+       <tr key={i}>
+         <td>{item.name}</td>
+         <td>{item.email}</td>
+         <td>{item.contact}</td>
+        </tr>:null
+       
       ))}
-      </table>
+      </tbody>
+      </Table>
     </div>
   );
 }
