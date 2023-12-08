1bf4cbfd4268f1f3d4c7b9455a97fba41f3062df
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-12-08 22:46:17 +0530

    VIDEO-47

--- src/App.js
+++ src/App.js
@@ -2,29 +2,42 @@ import "./App.css";
 import React, {useRef, useState} from "react";
  
 function App(){
-  let inputRef = useRef(null)
-  let inputRef2 = useRef(null)
-
-  function submitForm(e){
-    e.preventDefault()
-    console.warn("input field 1 value : ", inputRef.current.value)
-    console.warn("input field 2 value : ", inputRef2.current.value)
-    let input3 = document.getElementById('input3').value
-    console.warn("input field 3 value :", input3)
-
-  }
-   
   return (
-    <div className="App">
-      <h1>UnControlled Component</h1>
-      <form onSubmit={submitForm}>
-      <input ref={inputRef} type="text"/><br/> <br/>
-      <input ref={inputRef2}type="text"/><br/> <br/>
-      <input id="input3" type="text"/><br/> <br/>
-      <button >Submit</button>
-      </form>
-    </div>
+  <div className="App">
+    <h1>HOC</h1>
+    <HOCRed cmp={Counter}/>
+    <HOCGreen cmp={Counter}/>
+    <HOCBlue cmp={Counter}/>
+  </div>
   );
 }
 
+function HOCRed(props)
+{
+  return <h2 style={{backgroundColor: 'red', width: 100}}>RED<props.cmp/></h2>
+}
+
+function HOCGreen(props)
+{
+  return <h2 style={{backgroundColor: 'green', width: 100}}>GREEN<props.cmp/></h2>
+}
+
+function HOCBlue(props)
+{
+  return <h2 style={{backgroundColor: 'blue', width: 100}}>BLUE<props.cmp/></h2>
+}
+
+function Counter()
+{
+  const [count,setCount] = useState(0)
+  return <div>
+    <h3>{count}</h3>
+    <button onClick={()=> setCount(count+1)}>UPDATE</button>
+  </div>
+}
+
 export default App;
+  
+
+  
+    
