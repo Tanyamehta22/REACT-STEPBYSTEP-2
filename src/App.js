117b72a851a58abfa075e0d7d16f9f37ce31c261
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-12-08 18:32:29 +0530

    VIDEO-44

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import "./App.css";
-import React, {createRef} from "react";
+import React, {useRef} from "react";
+import User from './User11'
 function App(){
   let inputRef = useRef(null)
-  function handleInput()
+  function updateInput()
   {
-    console.warn("function call")
-    // inputRef.current.value="1000"
-    // inputRef.current.focus();
-    inputRef.current.style.display="none"
+    
+    inputRef.current.value="1000"
+    inputRef.current.focus();
+    inputRef.current.style.color="red"
   }
 
   return (
     <div className="App">
-      <h1>useRef in React</h1>
-      <input type="text" ref={inputRef}/>
-      <button onClick={handleInput}>Handle Input</button>
+      <h1>forwardRef in React</h1>
+       <User ref={inputRef}/>
+      <button onClick={updateInput}>Update InputBox</button>
     </div>
   );
 }
