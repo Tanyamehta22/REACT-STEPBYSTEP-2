059cdfa36b046689078f816e505f55819b17070f
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-12-08 11:18:19 +0530

    VIDEO-41

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import "./App.css";
-import React, {PureComponent} from "react";
-import User from "./User10";
+import React, {useState, useMemo} from "react";
+  
+function App(){
+  const [count, setCount] = useState(0);
+  const [item,setItem] = useState(10);
+
+  const multiCountMemo = useMemo(function multiCount(){
+    console.warn("multiCount")
+    return count *5
+  }, [count])
  
-class App extends React.Component{
-  constructor()
-  {
-    super();
-    this.state={
-      count:1
-    }
-  }
-   
-  render()
-  { 
     return(
       <div className="App">
-        <User count={this.state.count}/>
-        <button onClick={()=> this.setState({count: this.state.count+1})}>Update Count</button>
+        <h1>USEMEMO HOOK IN REACT</h1>
+        <h2>COUNT :</h2>
+        <h2>ITEM :</h2>
+        <h2>{multiCountMemo}</h2>
+        <button onClick={() => setCount(count +1)}>UPDATE COUNT</button>
+        <button onClick={() => setItem(item*10)}>UPDATE ITEM</button>
+         
       </div>
     );
   }
-}
+
      
    
 
