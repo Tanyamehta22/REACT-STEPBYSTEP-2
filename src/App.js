c2d179ea40f2fe8cc6de638b60c805f0ae8ac044
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-12-08 20:32:04 +0530

    VIDEO-46

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import "./App.css";
-import React, {useState} from "react";
-import User from './User11'
+import React, {useRef, useState} from "react";
+ 
 function App(){
-  let [val, setVal] = useState("000")
-  let [item, setItem] = useState(" ")
-   
+  let inputRef = useRef(null)
+  let inputRef2 = useRef(null)
 
+  function submitForm(e){
+    e.preventDefault()
+    console.warn("input field 1 value : ", inputRef.current.value)
+    console.warn("input field 2 value : ", inputRef2.current.value)
+    let input3 = document.getElementById('input3').value
+    console.warn("input field 3 value :", input3)
+
+  }
+   
   return (
     <div className="App">
-      <h1>Controlled Component</h1>
-      <input type="text" value={val} onChange={(e)=> setVal(e.target.value)}/>
-      <h3>Value {val}</h3>
+      <h1>UnControlled Component</h1>
+      <form onSubmit={submitForm}>
+      <input ref={inputRef} type="text"/><br/> <br/>
+      <input ref={inputRef2}type="text"/><br/> <br/>
+      <input id="input3" type="text"/><br/> <br/>
+      <button >Submit</button>
+      </form>
     </div>
   );
 }
