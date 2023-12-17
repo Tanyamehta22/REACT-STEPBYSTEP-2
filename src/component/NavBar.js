864ffd56b08a04c709c9b3763a605759dddc3cd9
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-12-17 12:37:49 +0530

    VIDEO-52

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,13 +1,13 @@
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 
 function NavBar(){
     return(<div>
-        <ul>
+        <ul className='navbar'>
             {/* <li><Link to="/about">ABOUT</Link></li>
             <li><Link to="/">HOME</Link></li> */}
-            <li><a href='/'>Home</a></li>
-            <li><a href='/about'>About</a></li>
-            <li><a href='/contact'>Contact Us</a></li>
+            <li><NavLink className="nav-bar-link" style={{color:'red'}} to='/'>Home</NavLink></li>
+            <li><NavLink className="nav-bar-link"  to='/about'>About</NavLink>  </li>
+            <li><NavLink className="nav-bar-link"  to='/contact'>Contact Us</NavLink> </li>
             <li><a href='/User/anil'>Anil</a></li>
             <li><a href='/User/peter'>Peter</a></li>
             
