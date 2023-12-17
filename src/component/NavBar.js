578bdb752bf9b7a3921ed5186538ed2769d1df40
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-12-17 14:52:54 +0530

    VIDEO-53

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -3,11 +3,15 @@ import {Link, NavLink} from 'react-router-dom'
 function NavBar(){
     return(<div>
         <ul className='navbar'>
-            {/* <li><Link to="/about">ABOUT</Link></li>
-            <li><Link to="/">HOME</Link></li> */}
-            <li><NavLink className="nav-bar-link" style={{color:'red'}} to='/'>Home</NavLink></li>
-            <li><NavLink className="nav-bar-link"  to='/about'>About</NavLink>  </li>
-            <li><NavLink className="nav-bar-link"  to='/contact'>Contact Us</NavLink> </li>
+           
+            <li><NavLink 
+            style={({isActive})=>{return {color:  isActive ? 'skyblue' :''}}}className="nav-bar-link" style={{color:'red'}} to='/'>Home</NavLink></li>
+
+            <li><NavLink
+            style={({isActive})=>{return{color:  isActive ? 'skyblue' :' '}}} className="nav-bar-link"  to='/about'>About</NavLink>  </li>
+
+            <li><NavLink
+            style={({isActive})=>{return{color:  isActive ? 'skyblue' :''}}} className="nav-bar-link"  to='/contact'>Contact Us</NavLink> </li>
             <li><a href='/User/anil'>Anil</a></li>
             <li><a href='/User/peter'>Peter</a></li>
             
