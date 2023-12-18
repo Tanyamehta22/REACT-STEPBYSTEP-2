7f6687336d7604f49124c2ea4f9c0888abf21014
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-12-18 06:47:56 +0530

    VIDEO-55

--- src/component/home.js
+++ src/component/home.js
@@ -1,10 +1,21 @@
+import{Link, useNavigate} from "reat-router-dom"
+
 function Home(){
+    const navigate = useNavigate();
+    const NavToPage=(url)=>{
+        navigate("")
+    }
+    
     return(
         <div>
             <h1>HOME PAGE</h1>
             <p>This is a Home Page of our awesome App</p>
             <p>And here we are learning about Router</p>
             <Link to="/about">Go to About page</Link>
+            <br/>
+            <button onClick={()=>NavToPage('/about')}>GO TO ABOUT PAGE</button>
+            <br/>
+            <button onClick={()=>NavToPage('/filter')}>GO TO FILTER PAGE</button>
         </div>
     )
         
