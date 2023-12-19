db2732589b02ecef29c3dc3f9712c7c1e548c6d4
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-12-19 23:33:23 +0530

    VIDEO-61

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
  import './App.css';
  import React from 'react'
- import {Link, Route } from 'react-router-dom'
- import Nav from './Nav'
- import Home from './component/home';
- import about from './component/about'
+ import {BrowserRouter as Route,Link,Route } from 'react-router-dom'
+  
  function App(){
   return (
     <div className='App'>
-       <Nav/>
-        <Route path="/about"><About/></Route>
-        <Route path="/" exact={true}><Home/></Route>
-        <br/>
+        <Router>
+            <Link to='/' >Home</Link><br/><br/>
+            <Link to='/' >About</Link><br/><br/>
+            <Link to='/login' >LOGIN</Link><br/><br/>
+
+
+            <switch>
+            <Route path='/' exact={true}><Home/></Route>
+            <Route path='/about'><About/></Route>
+
+            <Route path='*'><PageNotFound/></Route>
+            </switch>
+
+
+        </Router>
+        
        
     </div>
   );
  }
 
- 
+ function Home(){
+  return (
+    <div>
+      <h1>HOME PAGE</h1>
+      <p>This is home page of website</p>
+    </div>
+  )
+ }
+
+ function About(){
+  return (
+    <div>
+      <h1>ABOUT PAGE</h1>
+      <p>This is about page of website</p>
+    </div>
+  )
+ }
+
+ function About(){
+  return (
+    <div>
+      <h1>ABOUT PAGE</h1>
+      <p>This is about page of website</p>
+    </div>
+  )
+ }
   
-   
+ 
+ function PageNotFound(){
+  return (<div>
+    <h1>404 Page</h1>
+    <p>This is Not found</p>
+  </div>)
+ }
 export default App;
   
 
