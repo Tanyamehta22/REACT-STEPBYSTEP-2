79058d25273361a4bc360a5c19c956d3da2d623b
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-12-19 22:45:37 +0530

    VIDEO-60

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
-import "./App.css";
-import {BrowserRouter,Routes,Route, Navigate} from 'react-router-dom';
-import Home from './component/home'
-import About from '/component/about'
-import NavBar from "./components/Navbar";
-import Page404 from "./component/Page404";
-import User from "./component/User";
-import Contact from "./component/Contact";
- 
- 
-function App(){
+ import './App.css';
+ import React from 'react'
+ import {Link, Route } from 'react-router-dom'
+ import Nav from './Nav'
+ import Home from './component/home';
+ import about from './component/about'
+ function App(){
   return (
-    <div className="App">
-      <BrowserRouter>
-      <NavBar/>
-      <Routes>
-        <Route path='/' element={<Protected Component={Home}/>} />
-        <Route path='/login' element={<login/>} />
-        <Route path='/about' element={<About/>} />
-
-        <Route path='/user:name' element={<User/>} />
-
-        
-
-        <Route path='/*' element={<Navigate to="/" />} />
-
-        <Route path="/filter" element={<Filter/>}/>
-        
-        <Route path="/Contact/" element={<Contact/>}/>
-          <Route path="company" element={<Company/>}/>
-          <Route path="channel" element={<Channel/>}/>
-          <Route path="other" element={<Other/>}/>
-        
-      
-      </Routes>
-      </BrowserRouter>
+    <div className='App'>
+       <Nav/>
+        <Route path="/about"><About/></Route>
+        <Route path="/" exact={true}><Home/></Route>
+        <br/>
        
     </div>
   );
-}
+ }
+
+ 
   
    
 export default App;
