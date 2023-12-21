c3bd6b6c3d1caec8c4c34f587ee5f024504005d8
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-12-21 19:05:11 +0530

    VIDEO-65

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
  import './App.css';
  import React,{useEffect,useState} from 'react'
  function App(){
-  const [data, setData]= useState([])
-  useEffect(()=>{
-    fetch("http://localhost:4000/todo").then((result)=>{
+  const [name,setName] = useState("");
+
+  const [email,setEmail]= useState("");
+
+  const [mobile, setMobile]=useState("")
+
+  function saveUser()
+  {
+    console.warn(name,email,mobile);
+    let data={name,email,mobile}
+    fetch("http://localhost:4000/todo",{
+      method:'POST',
+      headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(data)
+    }).then((result)=>{
+      // console.warn("result",result);
       result.json().then((resp)=>{
-        // console.warn("result",resp)
-        setData(resp)
+        console.warn('resp',resp)
       })
     })
-  },[])
-  console.warn(data)
-  return(
+  }
+
+  return (
     <div className='App'>
-      <h1>GET API CALL</h1>
-      <table border='1'>
-        <tr>
-          <td>ID</td>
-          <td>Name</td>
-          <td>Email</td>
-          <td>Mobile</td>
-        </tr>
-        {
-          data.map((item)=>
-          <tr>
-            <td>{item.userId}</td>
-            <td>{item.name}</td>
-            <td>{item.email}</td>
-            <td>{item.mobile}</td>
-          </tr>
-          )
-        }
-      </table>
+      <h1>POST API EXAMPLE</h1>
+      <input type="text" value={name} onChange={(e)=>{setName(e.target.value)}} name="name"/><br/><br/>
+
+      <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}} name="email"/><br/><br/>
+
+      <input type="text" value={mobile} onChange={(e)=>{setMobile(e.target.value)}} name="mobile"/><br/><br/>
+
+      <button type="button" onClick={saveUser} >SAVE NEW USER</button>
+
     </div>
   )
  }
-
-
-           
-       
    
  
 export default App;
